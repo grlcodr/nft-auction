f389c441359a37ed7930c82dc079bee97f827d86
grlcodr <rachael@grlcodr.dev>
2022-06-11 20:18:37 +0100

    added nav and some main content inc countdown

---
+++ main.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    countdown();
+}, false);
+
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+ 
+function countdown() {
+    let day = document.getElementById('day');
+    let hour = document.getElementById('hour');
+    let min = document.getElementById('mins');
+    let sec = document.getElementById('secs');
+    let countDownDate = new Date("July 1, 2022 00:00:00").getTime();
+    let x = setInterval(function () {
+        let now = new Date().getTime();
+        let distance = countDownDate - now;
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        day.innerHTML = days;
+        hour.innerHTML = hours;
+        min.innerHTML = minutes;
+        sec.innerHTML = seconds;
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").style.display = "none"
+        }
+    }, 1000);
+}
